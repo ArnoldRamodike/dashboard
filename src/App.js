4220bf56a6567226e1041ab3ac7813ac4f631e37
ArnoldRamodike <riccoarnold23@gmail.com>
2023-01-10 15:43:30 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {BrowswerRouter, Routes, Route} from 'react-router-dom';
+import { FiSettings} from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 const App = () => {
   return(
-    <h1 className="underline text-3xl"> App</h1>
+    <div>
+      <BrowswerRouter>
+        <div className="flex related dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style={{zIndex: '100'}}>
+              <TooltipComponent content="settings" position="Top">
+                <button>
+                  <FiSettings/>
+                </button>
+              </TooltipComponent>
+            </div>
+        </div>
+      </BrowswerRouter>
+    </div>
   )
 }
 
